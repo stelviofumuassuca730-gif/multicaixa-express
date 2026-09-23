26d9417af2da891724cf0aa37c116be90f41260d
Stelvio Fumuassuca <stelviofumuassuca730@gmail.com>
2026-09-23 21:03:51 +0530

    Reduz linha vertical do comprovativo em 1mm

--- app/page.tsx
+++ app/page.tsx
@@ -141,7 +141,7 @@ async function buildReceiptPdf(op: Op) {
 
     pdf.setDrawColor(210, 0, 0)
     pdf.setLineWidth(1.1)
-    pdf.line(70, 62, 70, 134)
+    pdf.line(70, 62, 70, 133)
 
     rows.forEach(([label, value]) => {
       pdf.setFont('helvetica', 'bold')
